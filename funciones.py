6d22ebb065143f56b1830fae8b8216051fcf8439
alangajardomedina <alan.gajardo@profesor.duoc.cl>
2024-06-26 12:22:44 -0400

    inicio del menu con funciones sin desarrollo

---
+++ funciones.py
@@ -0,0 +1,14 @@
+libros = []
+#funciones
+def agregar_libro():
+    pass
+def mostrar_libros():
+    pass
+def buscar_libro():
+    pass
+def modificar_libro():
+    pass
+def exportar_libro():
+    pass
+def salir():
+    pass
---
+++ principal.py
@@ -0,0 +1,27 @@
+import time, os
+from funciones import *
+
+while True:
+    os.system('cls')
+    print("MENÚ BIBLIOTECA")
+    print("1. Agregar libro")
+    print("2. Mostrar libros")
+    print("3. Buscar libro por títul")
+    print("4. Modificar libro")
+    print("5. Exportar a JSON")
+    print("6. Salir")
+    opc = int(input("Ingrese opción: "))
+    os.system('cls')
+    if opc==1:
+        pass
+    elif opc==2:
+        pass
+    elif opc==3:
+        pass
+    elif opc==4:
+        pass
+    elif opc==5:
+        pass
+    else:
+        pass
+    time.sleep(3)
